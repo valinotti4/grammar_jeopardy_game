cc89a6ab9ad79582cbd16975462659e87bfe6739
Luciano Valinotti <lucianovalinotti@MacBook-Pro-de-Luciano.local>
2024-09-08 15:08:44 +0200

    Initial commit

---
+++ story_content/user.js
@@ -0,0 +1,30 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6WeqKHUHbw2":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  var player = GetPlayer();
+var score = player.GetVar("score")
+
+var ImsAPI = parent;
+if
+
+ImsAPI.SetScore(score, 100, 0);
+
+}
+
+};
